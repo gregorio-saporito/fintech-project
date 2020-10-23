4c4a6a323c6f5e5b2774379dc90ab41d39e76401
Gregorio Saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-10-23 18:12:17 +0200

    separate cleaning

--- main.R
+++ main.R
@@ -3,70 +3,20 @@ library(tidyverse)
 library(survival)
 library(ggfortify)
 
-API_Subcription <- read_csv("API_Subcription.csv") %>%
-  # remove column not needed
-  select(-NETWORK_ID) %>%
-  # remove part of string not needed from API
-  mutate(COMPLETED_STEP = str_remove_all(COMPLETED_STEP,
-                                     '/api/subscribe-|/api//subscribe-|.json')) %>%
-  # we want to track if the user goes back and forth counting the number of attempts
-  group_by(USER_ID,COMPLETED_STEP) %>%
-  arrange(USER_ID,COMPLETED_STEP,DATE_EVENT, ID) %>%
-  mutate(attempt = 1:n()) %>%
-  ungroup()
-
-# get ids of people that concluded
-ids <- API_Subcription %>% 
-  filter(COMPLETED_STEP == 'conclude') %>%
-  pull(USER_ID) %>% unique()
-
-
-# filter on users that concluded to see the steps
-API_Subcription %>%
-  filter(USER_ID %in% ids) %>%
-  arrange(USER_ID,ID)
-
-# some users just have one conclude without the previous steps
-
-# we need to count for how long each client has survived
-data <- tibble()
-count_ids = 0
-
-for(i in ids) {
-x <- API_Subcription %>%
-  filter(USER_ID == i)
-
-time_diff <- max(x$DATE_EVENT) - min(x$DATE_EVENT)
-
-extra_attempts <- x %>% 
-  group_by(COMPLETED_STEP) %>%
-  filter(attempt == max(attempt)) %>%
-  mutate(check = ifelse(attempt==1,0,attempt-1)) %>%
-  ungroup() %>%
-  summarise(extra_attempt = sum(check)) %>% pull()
-
-# status 1 if death occurred (conclude)
-
-status <- ifelse(nrow(filter(x,COMPLETED_STEP=='conclude'))==0,0,1)
-
-data <- data %>%
-  bind_rows(
-    tibble(
-      CLIENT_id = i,
-      # time in seconds
-      surv_time = as.double(time_diff),
-      status = status,
-      extra_attempts = extra_attempts
-    )
-  )
-# track percentage until complete
-count_ids <- count_ids + 1
-print(paste0("complete",round((count_ids/length(ids))*100),"%"))
-}
-
-data <- data %>%
+data <- read_csv("data.csv") %>%
   # extra attempts dummy
   mutate(extra_attempts_dummy = ifelse(extra_attempts==0,0,1))
+
+# many outliers for survival time
+# few clients go through the system for a long time
+boxplot(data$surv_time)
+# also a high number of extra attempts is rare
+boxplot(data$extra_attempts)
+
+# the distribution is asymmetric
+# Kernel density estimation
+plot(density(data$surv_time))
+plot(density(data$extra_attempts))
 
 # Kaplan-Meier estimates of the probability of survival over time
 km_fit <- survfit(Surv(surv_time, status) ~ 1, data=data)
